56ea54ee70c8189f62b10ccdbded6f0500264b2f
Andressa Urbano de Sousa Darze <andressa.darze@gmail.com>
2025-05-25 22:26:26 -0300

    init: adiciona projeto inicial do frontend em Next.JS

---
+++ um-sushi-frontend/app/home/page.tsx
@@ -0,0 +1,13 @@
+export default function HomePage() {
+  return (
+    <div style={{
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      height: '100vh',
+      fontSize: '2rem'
+    }}>
+      Hello UM Sushi
+    </div>
+  )
+}
