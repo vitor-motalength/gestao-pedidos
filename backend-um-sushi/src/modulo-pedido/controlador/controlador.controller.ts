6471ce03e6edab8cdf401f969b5e16a426c55cbb
gDias1905 <guidias1905@gmail.com>
2025-06-16 13:01:24 -0300

    Update controlador.controller.ts

--- backend-um-sushi/src/modulo-pedido/controlador/controlador.controller.ts
+++ backend-um-sushi/src/modulo-pedido/controlador/controlador.controller.ts
@@ -1,4 +1,26 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 
-@Controller('controlador')
-export class ControladorController { }
+@Controller('pedido')
+export class ControladorController {
+  private pedidos = [];
+
+  @Post()
+  criarPedido(@Body() body: any) {
+    const pedido = {
+      cliente: body.cliente,
+      itens: body.itens,
+    };
+
+    this.pedidos.push(pedido);
+
+    return {
+      mensagem: 'Pedido cadastrado com sucesso!',
+      pedido,
+    };
+  }
+
+  @Get()
+  listarPedidos() {
+    return this.pedidos;
+  }
+}
